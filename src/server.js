859943c7042f6e573db2eeb87f8dfdac73cd0452
briancastro-bc <brian.castro.bedoya@gmail.com>
2024-06-12 20:51:48 -0500

    feat(cors) added cors for frontend connectivity

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@
 // const express = require('express');
 
 // ESModule equivalent
+import cors from 'cors';
 import morgan from 'morgan';
 import express from 'express'; // Esto importa todo, lo que tiene la libreria/framework/etc
 import { createServer, } from 'http' // Cuando se utiliza {} se importa una funcion, clase, interfaz, etc. Especifica
@@ -13,12 +14,13 @@ async function bootstrap() {
   const app = express();
   const port = process.env.PORT ?? 3005;
   
+  app.use(cors());
   app.use(morgan('dev'));
   app.use(express.json());
 
   // Rutas
   app.use('/users', userRouter);
-  app.use('/books', userRouter);
+  // app.use('/books', userRouter);
   
   // The app will run in port - 3005
   const httpServer = createServer(app);
